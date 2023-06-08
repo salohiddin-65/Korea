831a2e792819ed7c1eb28398988b6cca05a01660
BlueApple856729 <126082098+BlueApple865729@users.noreply.github.com>
2023-06-07 23:20:06 -0800

    nav item setting:

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+const navitemlist = document.getElementsByClassName("navitem");
+for(let i = 0; i < navitemlist.length; i++)
+{
+    navitemlist[i].addEventListener("click", ()=> {
+        if(navitemlist[i].classList.contains("navitem-active"))
+            return true;
+        else {
+            for(let j = 0; j < navitemlist.length; j++){
+                navitemlist[j].classList.remove("navitem-active");
+            }
+            navitemlist[i].classList.add("navitem-active");
+        }
+    });
+}
+
+window.addEventListener('scroll', function() {
+	var element1 = document.querySelector('#about');
+	var position1 = element1.getBoundingClientRect();
+	var element2 = document.querySelector('#skill');
+	var position2 = element2.getBoundingClientRect();
+	var element3 = document.querySelector('#experience');
+	var position3 = element3.getBoundingClientRect();
+console.log(position1.bottom);
+	if(position1.bottom > this.window.innerHeight / 2) {
+        for(let j = 0; j < navitemlist.length; j++){
+            navitemlist[j].classList.remove("navitem-active");
+        }
+        navitemlist[0].classList.add("navitem-active");
+	}
+	else if(position2.top >= 0 && position2.bottom <= window.innerHeight) {
+        for(let j = 0; j < navitemlist.length; j++){
+            navitemlist[j].classList.remove("navitem-active");
+        }
+        navitemlist[1].classList.add("navitem-active");
+	}
+	else if(position2.bottom < 0) {
+        for(let j = 0; j < navitemlist.length; j++){
+            navitemlist[j].classList.remove("navitem-active");
+        }
+        navitemlist[2].classList.add("navitem-active");
+	}
+});
